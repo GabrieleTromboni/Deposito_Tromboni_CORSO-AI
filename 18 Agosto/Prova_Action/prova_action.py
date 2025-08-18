02d1a1b26258cfd9db0319c1cccbc373cf03839c
GabrieleTromboni <Gabriele.Tromboni@it.ey.com>
2025-08-18 14:52:13 +0200

    Update prova_action.py
    fix (Prova_Action): change function in prova_action.py

--- 18 Agosto/Prova_Action/prova_action.py
+++ 18 Agosto/Prova_Action/prova_action.py
@@ -5,13 +5,3 @@ z=add(x,y)
 print("Somma:",z)
 def add(a,b):
 return a+b
-def subtract(a,b):
-return a-b
-def multiply(a,b):
-return a*b
-def divide(a,b):
-if b==0:
-print("Errore: Divisione per zero")
-return None
-return a/b
-main()
