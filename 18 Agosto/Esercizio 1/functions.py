9642fc7985d672cf8b9379e9707c93b5985a6fdb
GabrieleTromboni <Gabriele.Tromboni@it.ey.com>
2025-08-18 12:26:08 +0200

    Spostamento file nella cartella e aggiunta funzione conteggio righe
    <add> (feat): "aggiungere script 'functions.py' e funzione di conteggio righe del dataframe'."

---
+++ 18 Agosto/Esercizio 1/functions.py
@@ -0,0 +1,21 @@
+'''
+Questo script contiene le funzioni per svolgere esercizio 1 del 18 Agosto.
+'''
+
+import pandas as pd
+import numpy as np
+
+def count_rows(df: pd.DataFrame) -> int:
+    '''
+    Questa funzione prende in input il dataframe derivante dal file di testo e mi restituisce il conteggio delle righe totali.
+    
+    Args:
+        pd.DataFrame: DataFrame da leggere e analizzare.
+        
+    Returns:
+        numero totale di righe del dataframe.
+        '''
+    
+    return len(df)
+    
+    
--- 18 Agosto/Esercizio 1/prova.py
+++ 18 Agosto/Esercizio 1/prova.py
@@ -12,7 +12,21 @@ Questo script svolge le richieste dell'esercizio 1 del 18 Agosto:
 import pandas as pd
 import numpy as np
 
-# LETTURA FILE 'input.txt'
-dati = pd.read_csv('input.txt', encoding='utf-8', delimiter='\t')
+# IMPORT SCRIPT
+from functions.py import count_rows
+
+def main(args):
+
+    # LETTURA FILE 'input.txt'
+    dati = pd.read_csv('input.txt', encoding='utf-8', delimiter='\t')
+
+    # CONTEGGIO RIGHE DEL DATAFRAME
+    total_rows = count_rows(dati)
+
+    # CONTEGGIO PAROLE TOTALI DEL DATAFRAME
+
+    # OUTPUT TOP-5 PAROLE:CONTEGGIO
+
+
 
 
