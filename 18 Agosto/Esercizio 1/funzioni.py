d5c052e47d5a5b60b939919f0f8b76eaa4765f0b
GabrieleTromboni <Gabriele.Tromboni@it.ey.com>
2025-08-18 12:55:37 +0200

    Update funzioni.py
    feat (prova-esercizio1): aggiunta funzione per ricevere la top5 in output

--- 18 Agosto/Esercizio 1/funzioni.py
+++ 18 Agosto/Esercizio 1/funzioni.py
@@ -4,6 +4,7 @@ Questo script contiene le funzioni per svolgere esercizio 1 del 18 Agosto.
 
 import pandas as pd
 import numpy as np
+from collections import Counter
 
 def count_rows(df: pd.DataFrame) -> int:
     '''
@@ -34,4 +35,25 @@ def count_words(df:pd.DataFrame) -> int:
         total_words += df[col].astype(str).str.split().str.len().sum()
     return total_words
     
+def count_top5(df:pd.DataFrame) -> int:
+    '''
+    Questa funzione prende in input il dataframe derivante da lfile di testp e mi restituisce il conteggio totale delle parole presenti.
+    
+    Args:
+        pd.DataFrame: DataFrame da leggere e analizzare.
+        
+    Returns:
+        dizionario di output con parola:conteggio formato per la top5.
+    '''
+
+    words = []
+    for col in df.columns:
+        words.extend(df[col].astype(str).str.split().sum())
+    
+    count_words = Counter(words)
+    top_5 = count_words.most_common(5)
+    
+    return {parola: conteggio for parola, conteggio in top_5}
+
+
     
