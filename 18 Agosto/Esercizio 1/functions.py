9495abbd652b3a2c90658297dd61edfdb36af303
GabrieleTromboni <Gabriele.Tromboni@it.ey.com>
2025-08-18 12:37:27 +0200

    aggiunta nuova funzoine per conteggio parole
    <feat> (branch): "aggiungere funzione del conteggio delle parole"

--- 18 Agosto/Esercizio 1/functions.py
+++ 18 Agosto/Esercizio 1/functions.py
@@ -17,5 +17,21 @@ def count_rows(df: pd.DataFrame) -> int:
         '''
     
     return len(df)
+
+def count_words(df:pd.DataFrame) -> int:
+    '''
+    Questa funzione prende in input il dataframe derivante da lfile di testp e mi restituisce il conteggio totale delle parole presenti.
+    
+    Args:
+        pd.DataFrame: DataFrame da leggere e analizzare.
+        
+    Returns:
+        numero totale di parole nel dataframe.
+    '''
+
+    total_words = 0
+    for col in df.columns:
+        total_words += df[col].astype(str).str.split().str.len().sum()
+    return total_words
     
     
--- 18 Agosto/Esercizio 1/prova.py
+++ 18 Agosto/Esercizio 1/prova.py
@@ -13,20 +13,23 @@ import pandas as pd
 import numpy as np
 
 # IMPORT SCRIPT
-from functions.py import count_rows
+from functions.py import count_rows, count_words
 
-def main(args):
+def main():
 
     # LETTURA FILE 'input.txt'
     dati = pd.read_csv('input.txt', encoding='utf-8', delimiter='\t')
 
     # CONTEGGIO RIGHE DEL DATAFRAME
     total_rows = count_rows(dati)
-
+    print(f"Il conteggio totale delle righe è: {total_rows}")
+    
     # CONTEGGIO PAROLE TOTALI DEL DATAFRAME
+    total_words = count_words(dati)
+    print(f"Il conteggio totale delle parole è: {total_words}")
 
     # OUTPUT TOP-5 PAROLE:CONTEGGIO
 
 
-
+if __name__ == 'main':
 
