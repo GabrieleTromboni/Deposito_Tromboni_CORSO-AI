ec41827d5d683c782b7dc96a3efd131d8f325c33
GabrieleTromboni <Gabriele.Tromboni@it.ey.com>
2025-08-18 12:14:51 +0200

    Create prova.py
    <add> (script): "creazione e iniziale impostazione dello script 'prova.py' per svolgere esercizio 1"

---
+++ prova.py
@@ -0,0 +1,18 @@
+'''
+Questo script svolge le richieste dell'esercizio 1 del 18 Agosto:
+
+- Leggere un file 'input.txt' (UTF-8)
+- Analizzare il file
+- Stampare numero totale di righe
+- Stampare numero totale di parole
+- Dare in output una top5 delle parole più frequenti nel formato parola:conteggio
+'''
+
+# IMPORT LIBRARIES
+import pandas as pd
+import numpy as np
+
+# LETTURA FILE 'input.txt'
+dati = pd.read_csv('input.txt', encoding='utf-8', delimiter='\t')
+
+
